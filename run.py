6c5205aaabb90fd325694b18e29f3cfd8a86ee64
FELD Boris <boris.feld@tinyclues.com>
2015-11-04 23:47:54 +0100

    Add a big warning about running saltpad in debug mode

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+import sys
 from saltpad.app import app
+
+host = app.config['HOST']
+
+if host == '0.0.0.0' and not '--insecure-debug-run' in sys.argv:
+    print ("You try to run saltpad in debug mode, please confirm by adding --insecure-debug-run option")
+    sys.exit(1)
+
+if host == '0.0.0.0':
+    print ("""Please be adviced that saltpad is running in debug mode and """
+           """listening to 0.0.0.0 which is very dangerous. If you're not """
+           """100% hundred sure of what to do, exit now.""")
 
 app.run(debug=True, host=app.config['HOST'])
