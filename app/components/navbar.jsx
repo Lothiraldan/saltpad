104cb0ea365c6a68bc9dac04e59cd3ea79537234
FELD Boris <boris.feld@tinyclues.com>
2015-12-18 09:15:14 +0100

    Clean the navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -13,95 +13,12 @@ export default class NavBar extends React.Component {
 
           <Link to="/" className="logo"><b>SALTPAD</b></Link>
 
-          <div className="nav notify-row" id="top_menu">
-            <ul className="nav top-menu">
-              <li className="dropdown">
-                <a href="/"><i className="fa fa-dashboard"><span> Dashboard</span></i></a>
-              </li>
-
-              <li className="dropdown">
-                <a href="/"><i className="fa fa-tasks"><span> Jobs</span></i></a>
-              </li>
-
-              <li className="dropdown">
-                <a href="/"><i className="fa fa-cubes"><span> Minions</span></i></a>
-              </li>
-
-
-
-
-                      <li id="header_inbox_bar" className="dropdown">
-                          <a data-toggle="dropdown" className="dropdown-toggle" href="#">
-                              <i className="fa fa-envelope-o"></i>
-                              <span className="badge bg-theme">5</span>
-                          </a>
-                          <ul className="dropdown-menu extended inbox">
-                              <div className="notify-arrow notify-arrow-green"></div>
-                              <li>
-                                  <p className="green">You have 5 new messages</p>
-                              </li>
-                              <li>
-                                  <a href="#">
-                                      <span className="photo"><img alt="avatar"></img></span>
-                                      <span className="subject">
-                                      <span className="from">Zac Snider</span>
-                                      <span className="time">Just now</span>
-                                      </span>
-                                      <span className="message">
-                                          Hi mate, how is everything?
-                                      </span>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#">
-                                      <span className="photo"><img alt="avatar"></img></span>
-                                      <span className="subject">
-                                      <span className="from">Divya Manian</span>
-                                      <span className="time">40 mins.</span>
-                                      </span>
-                                      <span className="message">
-                                       Hi, I need your help with this.
-                                      </span>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#">
-                                      <span className="photo"><img alt="avatar"></img></span>
-                                      <span className="subject">
-                                      <span className="from">Dan Rogers</span>
-                                      <span className="time">2 hrs.</span>
-                                      </span>
-                                      <span className="message">
-                                          Love your new Dashboard.
-                                      </span>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#">
-                                      <span className="photo"><img alt="avatar"></img></span>
-                                      <span className="subject">
-                                      <span className="from">Dj Sherman</span>
-                                      <span className="time">4 hrs.</span>
-                                      </span>
-                                      <span className="message">
-                                          Please, answer asap.
-                                      </span>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#">See all messages</a>
-                              </li>
-                          </ul>
-                      </li>
-                  </ul>
-              </div>
-
-            <div className="top-menu">
-              <ul className="nav pull-right top-menu">
-                    <li><a className="logout" onClick={LogoutUser}>Logout</a></li>
-              </ul>
-            </div>
-          </header>
-        )
+          <div className="top-menu">
+            <ul className="nav pull-right top-menu">
+                  <li><a className="logout" onClick={LogoutUser}>Logout</a></li>
+            </ul>
+          </div>
+        </header>
+      )
     }
 }
