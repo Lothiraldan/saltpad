506766442de92f4ccf5bef0b554140631e4f232f
Mathieu Bouchard <mathieu.bouchard@revenuquebec.ca>
2017-02-09 22:34:36 -0500

    Port to webpack2

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,7 +15,7 @@ module.exports = {
     filename: "/static/[name].js"
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     // new webpack.DefinePlugin({
     //   'process.env': {
     //     'NODE_ENV': JSON.stringify('production')
@@ -33,27 +33,35 @@ module.exports = {
     new ExtractTextPlugin("/static/styles.css"),
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx'],
     // Tell webpack to look for required files in bower and node
-    modulesDirectories: ['bower_components', 'node_modules']
+    modules: ['bower_components', 'node_modules']
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['babel?optional[]=runtime&stage=0'],
+        loaders: ['babel-loader?optional[]=runtime&stage=0'],
         include: path.join(__dirname, 'app')
       },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=/static/[name]-[hash].[ext]" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=/static/[name]-[hash].[ext]" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=/static/[name]-[hash].[ext]" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=/static/[name]-[hash].[ext]" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=/static/[name]-[hash].[ext]" },
+      { test: /\.jpg$/, loader: "file-loader?name=[name]-[hash].[ext]" },
+      { test: /\.png$/, loader: "file-loader?name=[name]-[hash].[ext]" },
       {
-        test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-      },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=/static/[name]-[hash].[ext]" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=/static/[name]-[hash].[ext]" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=/static/[name]-[hash].[ext]" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=/static/[name]-[hash].[ext]" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=/static/[name]-[hash].[ext]" },
-      { test: /\.jpg$/, loader: "file?name=[name]-[hash].[ext]" },
-      { test: /\.png$/, loader: "file?name=[name]-[hash].[ext]" },
+        rules: [
+          {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: "css-loader"
+            })
+          }
+        ]
+      }
     ]
   }
 };
